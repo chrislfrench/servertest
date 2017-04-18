6531d3fb8a29b19839ab2977e1222c0cae778adc
Chris French <clfrench84@gmail.com>
2017-04-17 19:45:09 -0700

    added function to return good or bad phrases

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ var PORT1 = 7000;
 
 var PORT2 = 7500;
 
+var goodPhrases = ["Good job", "Nice going!", "Good on you!"];
+
+var badPhrases = ["Bad job!", "You are terrible!", "Just give up!"];
+
 function handleRequest(request, response) {
 
-	response.end("it works!! Path hit: " + request.url + "You are a really cool guy");
+	response.end(getRandomPhrase("good"));
 }
 
 function handleRequest2(request, response) {
 
-	response.end("it works!! Path hit: " + request.url + "You aren't doing very well in life.");
+	response.end(getRandomPhrase("bad"));
 }
 
 var server1 = http.createServer(handleRequest);
@@ -20,9 +24,23 @@ var server2 = http.createServer(handleRequest2);
 
 
 server2.listen(PORT2, function() {
-	console.log("You are on port " + PORT2 + "and you are not doing very well in life.");
+	console.log("You are on port " + PORT2);
 })
 
 server1.listen(PORT1, function() {
-	console.log("You are on port " + PORT1 + "and you are a really cool guy.");
+	console.log("You are on port " + PORT1);
 })
+
+function getRandomPhrase (type) {
+	switch (type) {
+		case "good":
+		var index = Math.floor(Math.random()) * goodPhrases.length
+		return goodPhrases[index];
+		break;
+
+		case "bad":
+		var index = Math.floor(Math.random()) * badPhrases.length
+		return badPhrases[index];
+		break;
+	}
+}
